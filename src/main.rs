bba5c3f1f64bd0d63c0be5ff06239f1e12794bba
MagicLu(Chang Cun Lu) <46070909+MagicLu550@users.noreply.github.com>
2019-06-21 22:03:38 +0800

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,180 @@
 use std::io;
 use rand::Rng;
 
-
 //程序入口
 fn main() {
-
-lesson03();
+    
+    lesson06();
 
 }
+
+fn lesson06(){
+
+}
+fn first_word(s: &String) -> usize{
+    let bytes = s.as_bytes();
+
+    for(i,&item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+//参考和引用
+fn lesson05(){
+    let s1 = String::from("hello");
+    //请注意变量声明中的所有元组代码和函数返回值都消失了。
+    // 二，请注意，我们通过&s1进入 calculate_length，
+    // 并在它的定义，我们就&String不是 String。
+    let len = calculate_length(&s1);
+    //s->s1->堆内存
+    println!("{}{}",s1,len);
+
+    //不能直接修改不可变引用,添加mut
+    let mut s1 = String::from("hello");
+    test_2(&mut s1);
+    // 可变引用有一个很大的限制：对于特定范围内的特定数据，
+    // 只能有一个可变引用。此代码将错误
+    //let r1 = &mut s1;
+    //let r2 = &mut s1;
+    //println!("{}, {}", r1, r2);这里会报错
+    //与往常一样，我们可以使用花括号来创建一个新的范围，
+    // 允许多个可变引用，而不是同时引用：
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+    }
+    let r2 = &mut s;
+
+    //存在组合可变和不可变引用的类似规则，此代码导致错误
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    let r3 = &mut s;
+    //println!("{},{},and {}",r1,r2,r3);
+
+    //以下这种可以
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+
+    println!("{}",r1);
+
+    let r2 = &mut s;
+
+    println!("{}",r2);
+
+    //函数不允许返回&xx类型的值，因为当执行完代码时，引用引用的变量将会
+    //释放，那么返回的是一个无效的引用
+
+    //总结
+    //在任何时候，你可以拥有任何一个可变引用或任意数量不变引用。
+    //引用必须始终有效。
+
+}
+
+fn test_3(a:i32)->&i32{
+    return &a;
+}
+
+
+fn test_2(s:&mut String){
+    s.push_str("world");
+}
+
+fn calculate_length(s:&String) -> usize{
+    return s.len();
+}
+
+fn lesson04(){
+    /*
+    所有权的规则
+    Rust中的每个值都有一个称为其所有者的变量。
+    一次只能有一个所有者。
+    当所有者超出范围时，该值将被删除。
+    作为所有权的第一个例子，我们将研究一些变量的范围。
+    范围是项目有效的程序范围。假设我们有一个看起来像这样的变量：
+    */
+    let s= "hello";
+    //变量s引用字符串文字，其中字符串的值被硬编码到程序的文本中。
+    // 变量从声明它的位置到当前范围的结尾有效。清单4-1包含注释变量s有效的注释。
+    //当s谈到为范围，它是有效的。
+    //它一直有效，直到它超出范围。
+    let mut s = String::from("hello");
+
+    s.push_str(",world!");
+
+    println!("{}",s);
+    //将值绑定5到x; 然后使该值的副本，x并将其绑定到y，两个5值被推入堆栈
+    let x = 5;
+    let y = x;
+
+    // 而这里不是，是指向了同一块堆内存，指针指向第一个h
+    // 这里复制了堆栈的指针
+    let s1 = String::from("hello");
+    let s2 = s1;//这里，s1将会不起作用，不再有效，为了防止两次清理雷村，造成内存损坏
+    //会在超出变量的范围后调用drop函数，清理堆内存
+    //这里s1无效的情况，是移动了，也就是s1移动到了s2
+    println!("{}",s2);
+
+    let s3 = s2.clone();//复制堆数据，s2依然有效
+
+    let z = y;//堆栈数据的复制
+
+    let s  =String::from("hello");
+
+    takes_ownership(s);
+
+    //takes_ownership(s) 这里会报错，因为s指向的堆内存已经清除
+
+
+    let x = 5;
+
+    makes_copy(x);
+
+    //返回值可以转移所有权
+    let mut s1 = String::from("hello");
+    /*
+    变量的所有权每次都遵循相同的模式：将值赋给另一个变量会移动它。
+    当包含堆上数据的变量超出范围时，drop除非数据已被移动为另一个
+    变量所拥有，否则将清除该值。
+    */
+    s1 = return_value(s1);
+    println!("{}",s1);
+    /*
+        取得所有权，然后通过各种功能归还所有权有点单调乏味。
+        如果我们想让一个函数使用一个值但不取得所有权，该怎么办？
+        如果我们想要再次使用它们，除了我们可能想要返回的函数体所产生的任何数据之外，
+        我们传入的任何内容都需要传回。
+    */
+    let str1 = String::from("hello1");
+
+    let str2 = String::from("hello2");
+
+    let (str1_1,str2_1) = return_value_str_str(str1,str2);
+
+}
+
+fn return_value_str_str(str1:String,str2:String)-> (String,String){
+    (str1,str2)
+}
+
+fn return_value(str1:String) -> String{
+    return str1;
+}
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+// memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
 
 
 fn lesson03(){
@@ -42,17 +209,17 @@ fn lesson03(){
     //循环
     //永远死循环，直到手动退出
     let mut i = 0;
-    loop{
+    let i = loop{
         println!("again");
 
         if i>10{
             break i*2;//返回值
         }
         i+=1;
-    }
-    println!(i);
+    };
+    println!("{}",i);
 
-    let number = 0;
+    let mut number = 0;
     //有条件while
     while number>10{
         number+=1;
@@ -66,7 +233,7 @@ fn lesson03(){
     }
     //1-4反转
     for number in (1..4).rev(){
-        println!(number);
+        println!("{}",number);
     }
 
 
